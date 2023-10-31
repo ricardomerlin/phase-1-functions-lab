db17855d37e8f0601933b2af94f2161a87dc8c72
Ricardo Merlin <rcrdmerlin@gmail.com>
2023-10-31 13:42:31 -0400

    uploading changes to functions lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+let scuber = 42
+function distanceFromHqInBlocks(customer) {
+    return Math.abs(scuber-customer);
+}
+
+let blocks = distanceFromHqInBlocks();
+
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks)*264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return (Math.abs(start-destination))*264;
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0;
+    } else if (distanceTravelledInFeet(start, destination) > 400, distanceTravelledInFeet(start, destination) < 2000) {
+        return 0.02 * (distanceTravelledInFeet(start, destination)-400);
+    } else if (distanceTravelledInFeet(start, destination) > 2000, distanceTravelledInFeet(start, destination) < 2500) {
+        return 25;
+    } else if (distanceTravelledInFeet(start, destination) > 2500) {
+        return `cannot travel that far`;
+    }
+}
